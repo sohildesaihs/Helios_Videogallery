aa01daedc44b768918a813ea520f3dfe2758c54a
Sohil Desai <sohil.desai.hs@gmail.com>
2018-11-15 12:18:51 +0530

    Frontend templates issues fixed, saperated js and css files, updated block method to get result

--- app/code/community/Helios/Videogallery/Block/Index.php
+++ app/code/community/Helios/Videogallery/Block/Index.php
@@ -7,4 +7,35 @@
  */
 class Helios_Videogallery_Block_Index extends Mage_Core_Block_Template
 {
+    public function checkEnableDisable()
+    {
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_ENABLE);
+    }
+
+    public function getTitle()
+    {
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_TITLE);
+    }
+
+    public function getDescription()
+    {
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_DESCRIPTION);
+    }
+
+    public function getWidth()
+    {
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_VIDEO_WIDTH);
+    }
+
+    public function getHeight()
+    {
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_VIDEO_HEIGHT);
+    }
+
+    public function getAllData()
+    {
+        $videoCollection = Mage::getSingleton('videogallery/videogallery')->getCollection();
+
+        return $videoCollection;
+    }
 }
--- app/code/community/Helios/Videogallery/Helper/Data.php
+++ app/code/community/Helios/Videogallery/Helper/Data.php
@@ -7,36 +7,34 @@
  */
 class Helios_Videogallery_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    const CONFIG_GALLERY_ENABLE = 'gallerysetting/heliossetting/enabled';
+    const CONFIG_GALLERY_TITLE = 'gallerysetting/heliossetting/videotitle';
+    const CONFIG_GALLERY_DESCRIPTION = 'gallerysetting/heliossetting/videodesc';
+    const CONFIG_GALLERY_VIDEO_WIDTH = 'gallerysetting/heliossetting/videowidth';
+    const CONFIG_GALLERY_VIDEO_HEIGHT = 'gallerysetting/heliossetting/videoheight';
+
     public function checkEnableDisable()
     {
-        return Mage::getStoreConfig('gallerysetting/heliossetting/enabled');
-    }
-
-    public function getAllData()
-    {
-        $videoData = Mage::getSingleton('videogallery/videogallery')->getCollection();
-        $allData = $videoData->getData();
-
-        return $allData;
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_ENABLE);
     }
 
     public function getTitle()
     {
-        return '<u><h1 style="text-align: left">' . Mage::getStoreConfig('gallerysetting/heliossetting/videotitle') . '</h1></u>';
+        return '<u><h1 style="text-align: left">' . Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_TITLE) . '</h1></u>';
     }
 
     public function getDescription()
     {
-        return '<p style="text-align: left">' . Mage::getStoreConfig('gallerysetting/heliossetting/videodesc') . '</p>';
+        return '<p style="text-align: left">' . Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_DESCRIPTION) . '</p>';
     }
 
     public function getWidth()
     {
-        return Mage::getStoreConfig('gallerysetting/heliossetting/videowidth');
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_VIDEO_WIDTH);
     }
 
     public function getHeight()
     {
-        return Mage::getStoreConfig('gallerysetting/heliossetting/videoheight');
+        return Mage::getStoreConfig(Helios_Videogallery_Helper_Data::CONFIG_GALLERY_VIDEO_HEIGHT);
     }
 }
